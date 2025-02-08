010d7f4cad1f74d5a5969704bff9b50d715316f1
carolagottardo <carolamgottardo@gmail.com>
2025-02-08 05:29:49 +0100

    section content - added (home & about) // !!! display flex positioning required

--- src/components/Sectionsy/Sectionsy.jsx
+++ src/components/Sectionsy/Sectionsy.jsx
@@ -6,19 +6,31 @@ function Sectionsy() {
   return (
     <div className="sections-base">
       <div id="home" className="home-section">
-        <h1>Testing 1</h1>
+        <h1>Carola Gottardo</h1>
+        <p>Aspiring Fullstack Developer</p>
+        <div className="buttonie-container">
+          <button className="buttonie">Contact Me</button>
+          <button className="buttonie">Download CV</button>
+        </div>
       </div>
 
       <div id="about" className="about-section">
-        <h1>Testing 2</h1>
+        <h1>About</h1>
+        <h2>I'm an aspiring Fullstack Developer.</h2>
+        <p>
+          Recent Computer Science graduate from Università degli Studi
+          dell'Insubria, seeking to embark on my professional journey in the
+          workforce to continuously expand on my skills and knowledge in
+          Fullstack Development.
+        </p>
       </div>
 
       <div id="skills" className="skills-section">
-        <h1>Testing 3</h1>
+        <h1>Skills</h1>
       </div>
 
       <div id="contact" className="contact-section">
-        <h1>Testing 4</h1>
+        <h1>Contact</h1>
       </div>
     </div>
   );
